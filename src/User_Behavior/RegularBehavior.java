74e4a6c065e85ce813ca4598d01f1ebe4f06ec2e
Mobin <Mobinmojiri1234@gmail.com>
2025-04-18 11:32:35 +0330

    coding basics of RegularBehavior

--- src/User_Behavior/RegularBehavior.java
+++ src/User_Behavior/RegularBehavior.java
@@ -1,4 +1,35 @@
 package User_Behavior;
+import Exceptions.InvalidOperationException;
+import Songs.Music;
 
-public class RegularBehavior {
+
+public class RegularBehavior implements UserBehavior{
+
+    private int playinglimit = 5;
+
+
+    @Override
+    public void createPlaylist(String Title, User Owner) throws InvalidOperationException {
+
+        throw new InvalidOperationException("You cant create a playlist, you are a regular user. update to premium");
+
+    }
+
+    @Override
+    public void playMusic(Music music) {
+
+    }
+
+    @Override
+    public void buyPremium(User owner, int month) {
+
+    }
+
+    public int getPlayinglimit() {
+        return playinglimit;
+    }
+
+    public void setPlayinglimit(int playinglimit) {
+        this.playinglimit = playinglimit;
+    }
 }
