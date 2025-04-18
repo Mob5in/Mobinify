9c039c102bfd2b2e5f0da4431cc09dd9054275fb
Mobin <Mobinmojiri1234@gmail.com>
2025-04-18 14:41:54 +0330

    coding basics of playlist

--- src/Package/Songs/Playlist.java
+++ src/Package/Songs/Playlist.java
@@ -1,4 +1,47 @@
 package Package.Songs;
 
+import Package.Songs.User_Behavior.User;
+
+import java.util.ArrayList;
+
 public class Playlist {
+
+    private String title;
+    ArrayList<Music> playlist = new ArrayList<>();
+    User owner;
+
+
+    public Playlist(String title, User owner) {
+        this.title = title;
+        this.owner = owner;
+    }
+
+    public void editTitle(String newTitle, String password){
+
+    }
+
+    public void addMusic(Music music, String  password){
+
+    }
+
+    public void removeMusic(Music music, String password){
+
+    }
+    public ArrayList<Music> searchInPlaylist(){
+
+        return;
+    }
+
+
+    public void playPlaylist(){
+
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
 }
--- src/Package/Songs/User_Behavior/UserBehavior.java
+++ src/Package/Songs/User_Behavior/UserBehavior.java
@@ -4,11 +4,7 @@ import Package.Songs.Music;
 
 
 public interface UserBehavior {
-
     void createPlaylist (String Title, User Owner) throws InvalidOperationException;
-
     void playMusic (Music music) throws InvalidOperationException;
-
     void buyPremium (User owner, int month) throws InvalidOperationException;
-
 }
