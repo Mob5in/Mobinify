7214fe238479c6692d49cc12c3a55bd9f7a07edb
Mobin <Mobinmojiri1234@gmail.com>
2025-04-18 16:21:52 +0330

    coding meathods of Music

--- src/Package/Songs/Music.java
+++ src/Package/Songs/Music.java
@@ -1,8 +1,10 @@
 package Package.Songs;
 
+import Package.Songs.Exceptions.InvalidOperationException;
 import Package.Songs.User_Behavior.User;
 
 import java.util.ArrayList;
+import java.util.Objects;
 
 public class Music {
 
@@ -11,22 +13,48 @@ public class Music {
     private User singer;
     private int numberOfStream;
 
-
+    public Music(String title, User singer){
+        setTitle(title);
+        setSinger(singer);
+        allMusics.add(this);
+    }
 
     public static void play(Music music){
         System.out.println("The "+music.title+" is playing");
         music.setNumberOfStream(1 + music.getNumberOfStream());
     }
 
-    public static void search(){
-
+    public static ArrayList<Music> search(String title) throws InvalidOperationException {
+        ArrayList<Music> findedMusic = new ArrayList<>();
+        for(Music music: allMusics){
+            if(Objects.equals(music.title, title)){
+                findedMusic.add(music);
+            }
+        }
+        if(findedMusic.isEmpty()){
+            throw new InvalidOperationException("The music you are looking for we don't have");
+        }
+        return findedMusic;
     }
+
+    public static Music search(String title, String singer) throws InvalidOperationException {
+        for(Music music: allMusics){
+            if(Objects.equals(music.title, title) && Objects.equals(singer, music.getSinger().getUsername())){
+                return music;
+            }
+        }
+        throw new InvalidOperationException("The music you are looking for we don't have");
+    }
+
+
 
     //getters
     public String getTitle() {return title;}
     public int getNumberOfStream() {return numberOfStream;}
+    public User getSinger(){return this.singer;}
 
     //setters
+    public void setSinger(User singer){this.singer = singer;}
     public void setTitle(String title) {this.title = title;}
     public void setNumberOfStream(int numberOfStream) {this.numberOfStream = numberOfStream;}
 }
