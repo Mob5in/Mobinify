21c7436e8d73a7130c243c4cb0d4040616eb7ec2
Mobin <Mobinmojiri1234@gmail.com>
2025-04-18 16:09:30 +0330

    coding meathods of User

--- src/Package/Songs/User_Behavior/User.java
+++ src/Package/Songs/User_Behavior/User.java
@@ -2,6 +2,7 @@ package Package.Songs.User_Behavior;
 import Package.Songs.*;
 import Package.Songs.Exceptions.InvalidOperationException;
 import java.util.ArrayList;
+import java.util.Objects;
 
 public class User {
 
@@ -10,48 +11,73 @@ public class User {
     private String Password;
     private ArrayList<User> followerList = new ArrayList<>();
     private ArrayList<User> followingList = new ArrayList<>();
-    private UserBehavior behavior = new RegularBehavior();
+    private UserBehavior behavior;
     private ArrayList<Playlist> playlists = new ArrayList<>();
 
-    void follow (User user){
+
+    public User(String username, String password) throws InvalidOperationException {
+        setUsername(username);
+        setPassword(password);
+        this.behavior = new RegularBehavior();
+        allUsers.add(this);
+    }
+
+
+    void follow (User user) throws InvalidOperationException {
+
+        if(followerList.contains(user)){
+            throw new InvalidOperationException("You already have followed this user");
+        }
+        if(user == this){
+            throw new InvalidOperationException("You cant follow yourself");
+        }
+
+        this.followerList.add(user);
+        user.followerList.add(this);
 
     }
+
 
     void createPlaylist (String Title, User Owner) throws InvalidOperationException {
         this.behavior.createPlaylist(Title, Owner);
     }
 
-    void playMusic (Music music){
 
+    void playMusic (Music music) throws InvalidOperationException {
+        this.behavior.playMusic(music);
     }
 
-    void buyPremium (User owner, int month){
 
-    }
-
-    public static ArrayList<User> getAllUsers() {
-        return allUsers;
-    }
-
-    public static void setAllUsers(ArrayList<User> allUsers) {
-        User.allUsers = allUsers;
+    void buyPremium (User owner, int month) throws InvalidOperationException {
+        this.behavior.buyPremium(owner, month);
     }
 
 
     // getters
     public String getUsername() {return username;}
-    public String getPassword() {return Password;}
+    public String getPassword() {return Password;}// do we need this?
     public ArrayList<User> getFollowerList() {return followerList;}
     public ArrayList<User> getFollowingList() {return followingList;}
     public UserBehavior getBehavior() {return behavior;}
     public ArrayList<Playlist> getPlaylists() {return playlists;}
-
+    public ArrayList<User> getAllUsers() { return allUsers}
     //setters
-    public void setUsername(String username) {this.username = username;}
-    public void setPassword(String password) {Password = password;}
-    public void setFollowerList(ArrayList<User> followerList) {this.followerList = followerList;}
-    public void setFollowingList(ArrayList<User> followingList) {this.followingList = followingList;}
-    public void setBehavior(UserBehavior behavior) {this.behavior = behavior;}
-    public void setPlaylists(ArrayList<Playlist> playlists) {this.playlists = playlists;}
+    public void setUsername(String username) throws InvalidOperationException {
+        for(User user: allUsers){
+            if(Objects.equals(user.getUsername(), username)){
+                throw new InvalidOperationException("the username you took is already taken");
+            }
+        }
+        this.username = username;
+    }
+
+    public void setPassword(String password) throws InvalidOperationException {
+        if(password.length() < 8){
+            throw new InvalidOperationException("The password is too weak");
+        }
+        Password = password;
+    }
+
+
 
 }
