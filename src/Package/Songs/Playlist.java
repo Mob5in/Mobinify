36ede69d271c51a195ad667aed7fc48c55c87189
Mobin <Mobinmojiri1234@gmail.com>
2025-04-18 16:58:37 +0330

    coding methods of playlist

--- src/Package/Songs/Playlist.java
+++ src/Package/Songs/Playlist.java
@@ -1,16 +1,17 @@
 package Package.Songs;
 
+import Package.Songs.Exceptions.InvalidOperationException;
 import Package.Songs.User_Behavior.User;
-
 import java.util.ArrayList;
+import java.util.Objects;
+
 
 public class Playlist {
 
     //fields
     private String title;
-    ArrayList<Music> playlist = new ArrayList<>();
+    private ArrayList<Music> playlist = new ArrayList<>();
     User owner;
-
 
 
     //methods
@@ -19,25 +20,60 @@ public class Playlist {
         this.owner = owner;
     }
 
-    public void editTitle(String newTitle, String password){
-
+    public void editTitle(String newTitle, String password) throws InvalidOperationException {
+        if(!Objects.equals(owner.getPassword(), password)){
+            throw new InvalidOperationException("Wrong password");
+        }
+        this.setTitle(newTitle);
     }
 
-    public void addMusic(Music music, String  password){
 
+    public void addMusic(Music music, String  password) throws InvalidOperationException {
+        if(!Objects.equals(this.owner.getPassword(), password)){
+            throw new InvalidOperationException("Wrong password");
+        }
+        playlist.add(music);
     }
 
-    public void removeMusic(Music music, String password){
-
+    public void removeMusic(Music music, String password) throws InvalidOperationException {
+        if(!Objects.equals(this.owner.getPassword(), password)){
+            throw new InvalidOperationException("Wrong password");
+        }
+        if(this.playlist.contains(music)){
+            this.playlist.remove(music);
+        }else{
+            throw new InvalidOperationException("the music is not in the play list at all");
+        }
     }
-    public ArrayList<Music> searchInPlaylist(){
+    public ArrayList<Music> searchInPlaylist(String title) throws InvalidOperationException {
+        ArrayList<Music> findedMusic = new ArrayList<>();
+        for(Music music: this.playlist){
+            if(Objects.equals(music.getTitle(), title)){
+                findedMusic.add(music);
+            }
+        }
+        if(findedMusic.isEmpty()){
+            throw new InvalidOperationException("The music you are looking for is not in the playlist");
+        }
+        return findedMusic;
+    }
 
-        return;
+
+    public Music searchInPlaylist(String title, String singer) throws InvalidOperationException {
+
+        for(Music music: this.playlist){
+            if(Objects.equals(music.getTitle(), title) && Objects.equals(singer, music.getSinger().getUsername())){
+                return music;
+            }
+        }
+        throw new InvalidOperationException("The music you are looking for we don't have");
     }
 
 
     public void playPlaylist(){
-
+        for(Music music: this.playlist){
+            Music.play(music);
+        }
     }
 
     //getters
