501e5737c84761e45b903a60ec25723eb91c51df
Mobin <Mobinmojiri1234@gmail.com>
2025-04-18 11:38:45 +0330

    coding basics of PremiumBehavior.java

--- src/User_Behavior/PremiumBehavior.java
+++ src/User_Behavior/PremiumBehavior.java
@@ -1,4 +1,32 @@
 package User_Behavior;
 
-public class PremiumBehavior {
+import Exceptions.InvalidOperationException;
+import Songs.Music;
+
+public class PremiumBehavior implements UserBehavior{
+
+    private int month;
+
+    @Override
+    public void createPlaylist(String Title, User Owner) throws InvalidOperationException {
+
+    }
+
+    @Override
+    public void playMusic(Music music) throws InvalidOperationException {
+
+    }
+
+    @Override
+    public void buyPremium(User owner, int month) throws InvalidOperationException {
+        setMonth(month + getMonth());
+    }
+
+    public int getMonth() {
+        return month;
+    }
+
+    public void setMonth(int month) {
+        this.month = month;
+    }
 }
