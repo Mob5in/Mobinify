63107932e4b09e2a725ebf9a34adf937389e353d
Mobin <Mobinmojiri1234@gmail.com>
2025-04-18 13:06:08 +0330

    coding basics of Music

--- src/Songs/Music.java
+++ src/Songs/Music.java
@@ -1,4 +1,39 @@
 package Songs;
 
+import User_Behavior.User;
+
+import java.util.ArrayList;
+
 public class Music {
+
+    static ArrayList<Music> allMusics;
+    private String title;
+    private User singer;
+    private int numberOfStream;
+
+
+    public String getTitle() {
+        return title;
+    }
+
+    public static void play(Music music){
+        System.out.println("The "+music.title+" is playing");
+        music.setNumberOfStream(1 + music.getNumberOfStream());
+    }
+
+    public static void search(){
+
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getNumberOfStream() {
+        return numberOfStream;
+    }
+
+    public void setNumberOfStream(int numberOfStream) {
+        this.numberOfStream = numberOfStream;
+    }
 }
