557f1f790891ffa65f9a0d45c835b50ef4e4ded2
Mobin <Mobinmojiri1234@gmail.com>
2025-04-18 11:24:44 +0330

    add throwing exceptions in functions

--- src/User_Behavior/UserBehavior.java
+++ src/User_Behavior/UserBehavior.java
@@ -1,13 +1,14 @@
 package User_Behavior;
+import Exceptions.InvalidOperationException;
 import Songs.Music;
 
 
 public interface UserBehavior {
 
-    void createPlaylist (String Title, User Owner);
+    void createPlaylist (String Title, User Owner) throws InvalidOperationException;
 
-    void playMusic (Music music);
+    void playMusic (Music music) throws InvalidOperationException;
 
-    void buyPremium (User owner, int month);
+    void buyPremium (User owner, int month) throws InvalidOperationException;
 
 }
