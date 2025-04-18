4705d2856c74bf7f92bd1c9792150c2b25feefa9
Mobin <Mobinmojiri1234@gmail.com>
2025-04-18 11:17:35 +0330

    coding UserBehavior

--- src/User_Behavior/UserBehavior.java
+++ src/User_Behavior/UserBehavior.java
@@ -1,4 +1,13 @@
 package User_Behavior;
+import Songs.Music;
+
 
 public interface UserBehavior {
+
+    void createPlaylist (String Title, User Owner);
+
+    void playMusic (Music music);
+
+    void buyPremium (User owner, int month);
+
 }
