8632a38332be056cf56a850c8bf233d90e50cf1c
Mobin <Mobinmojiri1234@gmail.com>
2025-04-18 15:17:50 +0330

    coding RegularBehavior methods

--- src/Package/Songs/User_Behavior/RegularBehavior.java
+++ src/Package/Songs/User_Behavior/RegularBehavior.java
@@ -10,24 +10,35 @@ public class RegularBehavior implements UserBehavior{
 
     @Override
     public void createPlaylist(String Title, User Owner) throws InvalidOperationException {
-
         throw new InvalidOperationException("You cant create a playlist, you are a regular user. update to premium");
-
     }
+
 
     @Override
-    public void playMusic(Music music) {
+    public void playMusic(Music music) throws InvalidOperationException {
 
+        if(this.getPlayinglimit() > 0 ){
+            System.out.println("The "+music.getTitle()+" is playing");
+            Music.play(music);
+            this.setPlayinglimit( getPlayinglimit() - 1);
+
+        } else if (this.getPlayinglimit() <= 0) {
+            throw new InvalidOperationException("You have reached your play limit");
+
+        }
     }
+
 
     @Override
     public void buyPremium(User owner, int month) {
-
+        owner.setBehavior(new PremiumBehavior(month));
     }
+
 
     public int getPlayinglimit() {
         return playinglimit;
     }
+
 
     public void setPlayinglimit(int playinglimit) {
         this.playinglimit = playinglimit;
